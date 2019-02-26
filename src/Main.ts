59dbf2c9a3a6740fed5902ea76c82fc5eab04154
Billow Fay <billowfay@hahatekiMacBook-Pro.local>
2019-02-26 11:20:49 +0800

    f

--- src/Main.ts
+++ src/Main.ts
@@ -31,8 +31,7 @@ class Main extends eui.UILayer {
     private async runGame() {
         await this.loadResource()
         this.createGameScene();
-        const result = await RES.getResAsync("description_json")
-        this.startAnimation(result);
+        const result = await RES.getResAsync("description_json");
         await platform.login();
         const userInfo = await platform.getUserInfo();
         console.log(userInfo);
